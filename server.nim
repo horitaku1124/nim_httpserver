0c34767488cf487f553a6dfe54d12fffe8bb4e41
Takuya Horimatsu <new_woory@hotmail.com>
2017-11-01 22:11:42 +0900

    input / output sample

--- server.nim
+++ server.nim
@@ -1,19 +1,43 @@
 import net
+import strutils
 
+var MyPort = 8000
+var MyHost = "127.0.0.1"
 var socket = newSocket()
-socket.bindAddr(Port(8000),"127.0.0.1")
+socket.bindAddr(Port(MyPort), MyHost)
 socket.listen()
 
 var client = newSocket()
 var address = ""
 var lineBuff = ""
+echo "Started at http://", MyHost, ":", MyPort, "/"
 while true:
+  echo "wait"
   socket.acceptAddr(client, address)
 
-  client.readLine(lineBuff)
-  echo lineBuff
-  client.send("HTTP 200 OK\r\n\r\n")
+  var requestLines: seq[string] = @[]
+  var responseHeaders: seq[string] = @[]
+  while true:
+    client.readLine(lineBuff)
+    if lineBuff == "":
+      break
+
+    if lineBuff == "\r\n":
+      break
+
+    requestLines.add(lineBuff)
+    echo "[", lineBuff, "]"
+  
+  responseHeaders.add("HTTP 200 OK")
+  responseHeaders.add("Connection: close")
+  
+  for line in responseHeaders:
+    client.send(line)
+    client.send("\r\n")
+
+  client.send("\r\n")
   client.close()
+  echo "closed"
 
 socket.close()
 echo "Closed Socket"

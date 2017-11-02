de666b0ee83ef819f126e61b717618f6b3dc8876
Takuya Horimatsu <new_woory@hotmail.com>
2017-11-02 20:38:08 +0900

    respond to exists file

--- server.nim
+++ server.nim
@@ -1,8 +1,11 @@
 import net
 import strutils
+import streams
+import os
 
 var MyPort = 8000
 var MyHost = "127.0.0.1"
+var DocumentRoot = "./public_html"
 var socket = newSocket()
 socket.bindAddr(Port(MyPort), MyHost)
 socket.listen()
@@ -28,14 +31,52 @@ while true:
     requestLines.add(lineBuff)
     echo "[", lineBuff, "]"
   
-  responseHeaders.add("HTTP 200 OK")
-  responseHeaders.add("Connection: close")
-  
-  for line in responseHeaders:
-    client.send(line)
-    client.send("\r\n")
+
+  var protocols = split(requestLines[0], " ")
+  var method1 = protocols[0]
+  var path = protocols[1]
+  echo "Path=", path
+  if path.endsWith("/"):
+    path.add("index.html")
+
+  var filePath = DocumentRoot
+  filePath.add(path)
+  var responseBody:string = nil
+  if fileExists(filePath):
+    var fs = newFileStream(filePath, fmRead)
+    responseBody = fs.readAll()
+
+    responseHeaders.add("HTTP 200 OK")
+    responseHeaders.add("Connection: close")
+    responseHeaders.add("Server: NHS")
+    responseHeaders.add("Content-Length: " & responseBody.len().intToStr())
+    var contentType = "Content-Type: "
+    if filePath.endsWith(".html"):
+      responseHeaders.add(contentType & "text/html")
+    else:
+      responseHeaders.add(contentType & "application/octet-stream")
+
+  else:
+    responseHeaders.add("HTTP 404 Not Found")
+    responseHeaders.add("Server: NHS")
+    responseHeaders.add("Connection: close")
+
+
+
+  if method1 == "GET":
+    echo protocols
+    echo "Method:", method1
+    
+    for line in responseHeaders:
+      client.send(line)
+      client.send("\r\n")
 
   client.send("\r\n")
+
+  if responseBody != nil:
+    client.send(responseBody)
+
+
   client.close()
   echo "closed"
 

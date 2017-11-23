a33ca0b8c936b812a41cbf9becf635f03070ecde
Takuya Horimatsu <new_woory@hotmail.com>
2017-11-23 14:10:40 +0900

    support Keep-Alive

--- nim_httpserver2.nim
+++ nim_httpserver2.nim
@@ -6,8 +6,14 @@ import net
 import strutils
 import times
 import yaml.serialization, streams
+import tables
 
 import http_tools
+
+#
+# @see https://nim-lang.org/docs/asyncnet.html
+#
+
 
 type ServerConfig = object
   port : int32
@@ -25,67 +31,97 @@ var MyHost = serverConfig1.hostname
 var DocumentRoot = serverConfig1.document_root
 echo "Started at http://", MyHost, ":", MyPort, "/"
 
-var clients {.threadvar.}: seq[AsyncSocket]
+# var clients {.threadvar.}: seq[AsyncSocket]
 
 proc processClient(client: AsyncSocket) {.async.} =
-
-  var requestLines: seq[string] = @[]
-  var responseHeaders: seq[string] = @[]
+  var maxKeepAlive = 100
   while true:
-    let line = await client.recvLine()
-    if line == "" or line == "\r\n":
+    if client.isClosed:
       break
 
-    requestLines.add(line)
-    echo "[", line, "]"
+    if maxKeepAlive < 1:
+      client.close()
+    
+    var requestLines: seq[string] = @[]
+    var responseHeaders: seq[string] = @[]
+    while true:
+      let line = await client.recvLine()
+      if line == "" or line == "\r\n":
+        break
 
-  if requestLines.len() == 0:
-    return
-    # for c in clients:
-    #   await c.send(line & "\c\L")
-  
-  var protocols = split(requestLines[0], " ")
-  var method1 = protocols[0]
-  var gmtDate = timeToGmtString(getTime())
-  echo "Path=", protocols[1]
+      requestLines.add(line)
+      # echo "[", line, "]"
 
-  var filePath = resolveRealFilePath(protocols[1], DocumentRoot)
-  var responseBody:string = nil
-  if fileExists(filePath):
-    var fs = newFileStream(filePath, fmRead)
-    responseBody = fs.readAll()
+    if requestLines.len() == 0:
+      return
+      # for c in clients:
+      #   await c.send(line & "\c\L")
+    
+    var protocols = split(requestLines[0], " ")
+    var method1 = protocols[0]
+    var gmtDate = timeToGmtString(getTime())
+    echo "Path=", protocols[1]
 
-    responseHeaders.add("HTTP/1.1 200 OK")
-    responseHeaders.add("Connection: close")
-    responseHeaders.add("Date: " & gmtDate)
-    responseHeaders.add("Server: NHS")
-    responseHeaders.add("Content-Length: " & responseBody.len().intToStr())
+    var headersTable = initTable[string, string]()
+    for i in countup(1, requestLines.len() - 1):
+      let line2s = requestLines[i].split(": ")
+      headersTable[line2s[0]] = line2s[1]
 
-    var contentType = "Content-Type: " & http_tools.decideContentType(filePath, DefaultEncode)
-    responseHeaders.add(contentType)
+    echo " HEAD -> ", headersTable
+    var useKeepAlive = headersTable.contains("Connection") and headersTable["Connection"] == "keep-alive"
 
-  else:
-    responseHeaders.add("HTTP/1.1 404 Not Found")
-    responseHeaders.add("Server: NHS")
-    responseHeaders.add("Connection: close")
+    if useKeepAlive:
+      echo "KeepAlive -> ", headersTable["Connection"]
+      
+
+    var filePath = resolveRealFilePath(protocols[1], DocumentRoot)
+    var responseBody:string = nil
+    if fileExists(filePath):
+      var fs = newFileStream(filePath, fmRead)
+      responseBody = fs.readAll()
+
+      responseHeaders.add("HTTP/1.1 200 OK")
+      if useKeepAlive:
+        responseHeaders.add("Connection: Keep-Alive")
+        responseHeaders.add("Keep-Alive: timeout=15, max=" & maxKeepAlive.intToStr())
+      else:
+        responseHeaders.add("Connection: close")
+
+      responseHeaders.add("Date: " & gmtDate)
+      responseHeaders.add("Server: NHS")
+      responseHeaders.add("Content-Length: " & responseBody.len().intToStr())
+
+      var contentType = "Content-Type: " & http_tools.decideContentType(filePath, DefaultEncode)
+      responseHeaders.add(contentType)
+
+    else:
+      responseHeaders.add("HTTP/1.1 404 Not Found")
+      responseHeaders.add("Server: NHS")
+      responseHeaders.add("Content-Length: 0")
+      if useKeepAlive:
+        responseHeaders.add("Connection: Keep-Alive")
+        responseHeaders.add("Keep-Alive: timeout=15, max=" & maxKeepAlive.intToStr())
+      else:
+        responseHeaders.add("Connection: close")
 
 
-  # echo "Method:", method1
-  if method1 == "GET" or method1 == "HEAD":
-    for line2 in responseHeaders:
-      await client.send(line2)
-      await client.send("\r\n")
-  
-  await client.send("\r\n")
+    # echo "Method:", method1
+    if method1 == "GET" or method1 == "HEAD":
+      for line2 in responseHeaders:
+        await client.send(line2 & "\r\n")
+        echo line2
+    
+    await client.send("\r\n")
 
-  if responseBody != nil and method1 != "HEAD":
-    await client.send(responseBody)
+    if responseBody != nil and method1 != "HEAD":
+      await client.send(responseBody)
+    
+    maxKeepAlive = maxKeepAlive - 1
+    echo "Finished\n\n\n"
 
-  # client.close()
   echo "closed"
 
 proc serve() {.async.} =
-  # clients = @[]
   var server = newAsyncSocket()
   server.setSockOpt(OptReuseAddr, true)
   server.bindAddr(Port(MyPort))
@@ -93,8 +129,6 @@ proc serve() {.async.} =
   
   while true:
     let client = await server.accept()
-    # clients.add client
-    
     asyncCheck processClient(client)
 
 asyncCheck serve()
